2881e489c55cf8ba63ba2606cedbabde03295b50
Marcel Spitau <blog@spitau.de>
2020-04-11 15:45:48 +0200

    time ist displayed and the title is displayed

--- timer.sh
+++ timer.sh
@@ -10,7 +10,8 @@
 # [[ $DEBUG ]] && echo ${DEBUG} "Nachricht"
 
 
-
+# Progressbar credits to:
+# https://github.com/fearside/ProgressBar/
 
 # 1. Create ProgressBar function
 # 1.1 Input is currentState($1) and totalState($2)
@@ -26,8 +27,7 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /▋}${_empty// /-}] ${_progress}%%"
-
+printf "\r$title[${_fill// /▋}${_empty// /-}] ${_progress}%% - $(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)"
 }
 
 clear
@@ -38,12 +38,13 @@ alarmton=/home/marcel/Dropbox/3_audio/3+1_bell.ogg
 sekunden=${1}
 date1=$((`date +%s` + $sekunden));
 
-[[ ! -z ${2} ]] && echo "# "${2}
+[[ ! -z ${2} ]] && title=""${2}": " || title="Timer: "
 
 while [ "$date1" -ge `date +%s` ]; do
 	vergangenezeit=$((sekunden-(date1-`date +%s`)))
 	ProgressBar ${vergangenezeit} ${sekunden}
-    # printf " - "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)"\r";
+    # printf " - "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S);
 	sleep 0.5
 done
+
 cvlc ${alarmton} 2>&1 > /dev/null
