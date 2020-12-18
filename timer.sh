8285f480b82725d2e14bb87f0a288007ab3a4c6e
Marcel Spitau <blog@spitau.de>
2020-12-18 08:41:32 +0100

    clvlc mit play ersetzt

--- timer.sh
+++ timer.sh
@@ -11,7 +11,6 @@
 figlet=`which figlet`
 [[ -f $figlet ]] && endetxt="figlet -c" || endetxt="echo"
 
-
 # Progressbar credits to:
 # https://github.com/fearside/ProgressBar/
 
@@ -25,7 +24,6 @@ function ProgressBar {
 # Build progressbar string lengths
     _fill=$(printf "%${_done}s")
     _empty=$(printf "%${_left}s")
-
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [########################################] 100%
@@ -54,4 +52,4 @@ echo " "
 echo " "
 $endetxt $title
 
-cvlc ${alarmton} 2>&1 > /dev/null
+play ${alarmton} 2>&1 > /dev/null
